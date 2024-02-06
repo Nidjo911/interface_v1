d94d5705249bc13eb2307d80181d27b5cd041165
Johnnson <af7er.g@gmail.com>
2024-02-06 22:35:48 +0100

    unit

---
+++ src/components/adventures/Unit_calc_1_toBeNamedL8er.jsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React, { useState } from 'react'
+
+export default function CalculationOne() {
+
+    const arrData = [];
+    
+
+    return (
+        <>
+            <h1>This component does calc for: ...</h1>
+        </>
+
+    )
+}
